8e43151cd04d280673ab55b2076ef3b8c2e4a725
WaterproofPaint22 <maiquanminh220407@gmail.com>
2025-11-18 22:48:26 +0700

    Add files via upload

--- btvn8.c
+++ btvn8.c
@@ -9,11 +9,11 @@ struct Student {
 int main() {
     struct Student students[50];
     int numStudents = 5;
-    students[0] = (struct Student){1, "Nguyen Van A", 20, "0901234567"};
-    students[1] = (struct Student){2, "Tran Thi B", 21, "0912345678"};
-    students[2] = (struct Student){3, "Le Van C", 22, "0923456789"};
-    students[3] = (struct Student){4, "Pham Thi D", 19, "0934567890"};
-    students[4] = (struct Student){5, "Hoang Van E", 23, "0945678901"};
+    students[0] = (struct Student){1, "Nguyen Van A", 20, "0911111111"};
+    students[1] = (struct Student){2, "Nguyen Van B", 21, "0922222222"};
+    students[2] = (struct Student){3, "Nguyen Van C", 22, "0933333333"};
+    students[3] = (struct Student){4, "Nguyen Van D", 19, "0944444444"};
+    students[4] = (struct Student){5, "Nguyen Van E", 23, "0955555555"};
     printf("Danh sach sinh vien truoc khi chen:\n");
     printf("%-5s %-20s %-5s %-15s\n", "ID", "Ten", "Tuoi", "So dien thoai");
     for (int i = 0; i < numStudents; i++) {
